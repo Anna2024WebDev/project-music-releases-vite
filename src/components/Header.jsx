276e5b9a750b31d9aee9a1b662eeac577dd7c42d
Anna <annah80@gmail.com>
2024-10-12 11:49:45 +0200

    added a header logotype and changed h1 and p texts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import "./Header.css"
+import logo from "../assets/logo.png"
 
 export const Header = () => {
   return (
     <>
       < header className="Header">
-        <h1>SoundScape</h1>
-        <p>Find your favorite music.</p>
+        <h1><img src={logo} alt="logo" className="SoundScape logo" />SoundScape</h1>
+        <p>Discover new music.</p>
       </header >
     </>
   );
